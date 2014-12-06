a54843f133cb935917cc382f6efa1b7de5a9ba4d
AlcatrazGR <alcatrazgr0@gmail.com>
2014-12-06 20:53:41 +0200

    1h fash submit password ('submit.php')

--- Game Vault/PHP_Scripts/submit.php
+++ Game Vault/PHP_Scripts/submit.php
@@ -1,20 +1,38 @@
 <?php
-	$username = $_POST['username'];
-	$password = $_POST['password'];
-	$rpassword = $_POST['repassword'];
 	
-	$email = $_POST['email'];
-	$emailService = $_POST['emailService'];
-	$email = $email."@".$emailService;
+	$requiredFields = array('username', 'password', 'repassword', 'emailService', 'sex', 'date', 'month', 'year', 'ImageToUpload');
+		
+	$error = false;
+	foreach($requiredFields as $field) {
+		if (empty($_POST[$field])) {
+			$error = true;
+		}
+	}	
 	
-	$sexType = $_POST['sex'];
+	if($error){
+		echo "You must first fill all the form fields";
+	}
+	else{
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		$rpassword = $_POST['repassword'];
 	
-	$date = $_POST['date'];
-	$month = $_POST['month'];
-	$year = $_POST['year'];
-	$bdate = $date."-".$month."-".$year;
-
-	echo $username." ".$password." ".$email." ".$sexType." ".$bdate." ";
+		$email = $_POST['email'];
+		$emailService = $_POST['emailService'];
+		$email = $email."@".$emailService;
+	
+		$sexType = $_POST['sex'];
+	
+		$date = $_POST['date'];
+		$month = $_POST['month'];
+		$year = $_POST['year'];
+		$bdate = $date."-".$month."-".$year;
+		
+		$userImage = $_POST['ImageToUpload'];
+		
+		echo $username." ".$password." ".$email." ".$sexType." ".$bdate." ".$userImage;
+	}
+		
 
 
 ?>
