b9801c358e51011eea78f62c4d06d6ea4f23bef8
AlcatrazGR <alcatrazgr0@gmail.com>
2014-12-17 10:58:10 +0200

    game data integrity kai game image integrity

--- Game Vault/PHP_Scripts/game.php
+++ Game Vault/PHP_Scripts/game.php
@@ -23,6 +23,57 @@
 			$this->gameCateg = $category;
 			$this->videoURL = $url;
 		}
+		
+		//Method that checks if the data given are Wright
+		public function gameDataIntegrity(){
+			$errorMessage="";
+			
+			if(strpos($this->videoURL, " ") !== false){
+				$errorMessage .= "The youtube video url you have given contains invalid characters! <br />";
+			}
+			if(strpos($this->videoURL, "http://") === false){
+				$errorMessage .= "The data you have given in the youtube video url is invalid ! <br />";
+			}
+			
+			return $errorMessage;
+		}
+		
+		//Method that checks the integrity of the game image
+		//size, extension etc.
+		public function gameImageIntegrity($file, $file_size, $file_ext){
+			$errorMessage = "";
+			$allowedExtensions = array("jpg","jpeg","gif","bmp","JPG","JPEG","GIF","BMP");
+
+			//Chechs if the extension of the image is allowed
+			if(in_array($file_ext, $allowedExtensions)){
+				//Checks the image size
+				if($file_size <= 697152){
+					//Checks if the directory for the game images exists, if not it creates it
+					if(!is_dir("Game/Images/")){
+						mkdir("Game/Images/", 0777, true);
+					}
+				}
+				else{
+					$errorMessage .= "The size of the image exceeds the limit !! <br />";
+				}
+			}
+			else{
+				$errorMessage .= "The extension of the file is invalid !! <br />";
+			}
+			
+			return $errorMessage;			
+		}
+		
+		//Method that checks if the data given already exist in DAO
+		public function gameDataExistInDatabase(){
+		
+		}
+
+		//Method that inserts the new data row in the table 'game'
+		public function insertGameToDatabase(){
+		
+		}
+		
 	
 	}
 
--- Game Vault/PHP_Scripts/gameSubmit.php
+++ Game Vault/PHP_Scripts/gameSubmit.php
@@ -34,11 +34,33 @@
 			$gameObj = new game();
 			$gameObj->setDataMembers($gameTitle, $minGameReq, $maxGameReq, $gameDescr, $file_name, 
 					$gamePlat, $gameCateg, $videoURL);
-			
+					
+			$result = $gameObj->gameDataIntegrity();
+			if($result == ""){
+				$file_size = $file['size'];
+				
+				//Works out the file extension
+				$file_ext = explode('.', $file_name);
+				$file_ext = strtolower(end($file_ext));
+				
+				$result = $gameObj->gameImageIntegrity($file, $file_size, $file_ext);
+				if($result == ""){
+					header( "refresh:5;url=../adminSubmitForm.php" );
+					echo $result."<br /> You will be redirected back to the page ...";	
+				}
+				else{
+				
+				}
+				
+			}
+			else{
+				header( "refresh:5;url=../adminSubmitForm.php" );
+				echo $result."<br /> You will be redirected back to the page ...";	;
+			}
 		}
 		else{
 			header( "refresh:3;url=../adminSubmitForm.php" );
-			echo "You didn't select any images";
+			echo "You didn't select any images<br /> You will be redirected back to the page ...";	;
 		}
 	
 	}
